6ce81e5d11d41d11a3ac8c5ed97da85d9fc17651
daivinsoncursos <daivinson@gmail.com>
2025-05-20 21:34:59 -0400

    api de bicicletas

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,4 +5,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([8.56430714840492, -71.2105544185014]).addTo(map);
+$.ajax({
+    dataType:"json",
+    url: "api/bicicletas",
+    success: function(result){
+       // console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion,{title:`${bici.id},${bici.color}`}).addTo(map);
+        })
+    }
+});
+
---
+++ routes/api/bicicletas.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bicicletaControllers =require('../../controllers/api/bicicletaControllerAPI');
+
+router.get('/',bicicletaControllers.bicicleta_list);
+router.post('/create',bicicletaControllers.bicicleta_create);
+router.delete('/delete',bicicletaControllers.biciclete_delete);
+
+module.exports = router;
